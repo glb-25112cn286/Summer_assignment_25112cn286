f271eb6f25cbbb89163c71eb82c2010b0320d509
glb-25112cn286 <shrutigupta241107@gmail.com>
2026-06-08 14:27:08 +0530

    Add files via upload

---
+++ Day_10.c/Day_10.4.c
@@ -0,0 +1,30 @@
+//Write a program to Print character pyramid. 
+//     A
+//    ABA
+//   ABCBA
+//  ABCDCBA
+// ABCDEDCBA
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=rows-i; j++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=i; j++)
+        {
+            printf("%c", j+64);
+        }
+        for(j=i-1; j>=1; j--)
+        {
+            printf("%c", j+64);
+        }
+        printf("\n");
+    }
+    return 0;
+}
